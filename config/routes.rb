568a0c5ac39c980663ec76e6ab89785191c08518
Stephen O'Reilly <oraghallaigh23@yahoo.ie>
2019-11-04 11:06:46 +0000

    added a store index action

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
-  get 'store/index'
+	root to: 'store#index', as: 'store'
 
-  resources :products
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+	resources :products
+	# For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
