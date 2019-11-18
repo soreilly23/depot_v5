e45c1bea0dda2842e28344976e2215b941a629bd
Stephen O'Reilly <oraghallaigh23@yahoo.ie>
2019-11-18 12:34:26 +0000

    user scaffold

--- app/models/order.rb
+++ app/models/order.rb
@@ -1,5 +1,4 @@
 class Order < ApplicationRecord
-	validates :pay_type, inclusion: pay_types.keys
 
 	has_many :line_items, dependent: :destroy
 	enum pay_type: {
@@ -7,6 +6,8 @@ class Order < ApplicationRecord
 		"Credit Card" => 1,
 		"Purchase Order" => 2
 	}
+
+	validates :pay_type, inclusion: pay_types.keys
 
 	def add_line_items_from_cart(cart)
 	  cart.line_items.each do |item|
