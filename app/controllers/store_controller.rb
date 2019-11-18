a0538200fad855eef2e949ef979323014a18636a
Stephen O'Reilly <oraghallaigh23@yahoo.ie>
2019-11-18 10:59:09 +0000

    included cart in store layout

--- app/controllers/store_controller.rb
+++ app/controllers/store_controller.rb
@@ -1,4 +1,6 @@
 class StoreController < ApplicationController
+  include CurrentCart
+  before_action :set_cart
   def index
   	@products = Product.order(:title)
   	@current_date_and_time = Time.now.strftime("%d/%m/%Y %H:%M")
